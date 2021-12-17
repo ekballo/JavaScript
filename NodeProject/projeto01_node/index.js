c0de220f1478a74bd78945e3d2ddee737548f4ae
ekballo <81698678+ekballo@users.noreply.github.com>
2021-12-17 13:04:38 -0300

    index

--- NodeProject/projeto01_node/index.js
+++ NodeProject/projeto01_node/index.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
+app.use('/public',express.static(path.join(__dirname,'public')));
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+
+    res.send('Carregado página incial.');
+
+
+});
+
 app.listen(5000, () =>{
     console.log('server rodando!');
 })
