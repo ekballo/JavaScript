df2baa8ed291b9fdaf5252918793573d10383e99
ekballo <81698678+ekballo@users.noreply.github.com>
2022-01-03 17:53:51 -0300

    index

--- projeto01_node/index.js
+++ projeto01_node/index.js
@@ -7,8 +7,12 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname,'public')));
 app.set('views', path.join(__dirname, 'views'));
 
+
+
+var tarefas = ['Arrumar a Sala', 'Comprar pão']
+
 app.get('/',(req,res)=>{
-    res.send('index',{});
+    res.send('index',{tarefaslist: tarefas});
 });
 
 app.listen(5000, () =>{
