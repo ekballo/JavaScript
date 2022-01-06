67d3b232e073ca88395ad5f4e18474c291f20212
ekballo <81698678+ekballo@users.noreply.github.com>
2022-01-06 15:51:07 -0300

    home

--- projeto01_node/PortaDeNoticiasEstatico/index.js
+++ projeto01_node/PortaDeNoticiasEstatico/index.js
@@ -18,7 +18,12 @@ app.set('views', path.join(__dirname, 'pages'));
 
 app.get('/',(req,res)=>{
     
-    res.render('home',{});
+    if(req.query.busca == null){
+        res.render('home',{});
+    }else{
+        res.send('Você buscou: '+req.query.buscar);
+    }
+    
 
 
 });
