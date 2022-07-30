f0110e3bb29b9a5cd09bbb92ae1426ac14f37173
Jefferson Garcia <ekball007@outlook.com>
2022-07-30 16:22:07 -0300

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+// getElementById()
+
+//const element = document.getElementById('blog-title')
+//console.log(element)
+
+const element = document.getElementsByClassName('one', 'two')
+console.log(element)
