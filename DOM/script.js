e82484a958edad984a77001029d33c65c76a3885
Jefferson Garcia <ekball007@outlook.com>
2022-08-01 07:43:09 -0300

    index

--- DOM/script.js
+++ DOM/script.js
@@ -3,5 +3,26 @@
 //const element = document.getElementById('blog-title')
 //console.log(element)
 
-const element = document.getElementsByClassName('one', 'two')
-console.log(element)
+// 1- const element = document.getElementsByClassName('one', 'two')
+// console.log(element)
+
+// 01/08/2022
+
+/* Manipulação de conteudo !
+TextContent
+
+const element = document.querySelector('h1')
+
+element.textContent += "Olá Devs!"
+console.log(element.textContent)
+
+const element = document.querySelector('h1')
+element.innerText = "Hey Devs !"
+
+const element = document.querySelector('h1')
+element.innerHTML = "Hey <small>Devs!</small>"
+*/
+
+const element = document.querySelector('input')
+
+element.value = "testado com sucesso !"
