c13e0d980630a9cdfb3e90ed6e485a37cfaa96e3
Jefferson Garcia <ekball007@outlook.com>
2022-08-15 06:57:08 -0300

    BestMe

---
+++ NodeJS/index.js
@@ -0,0 +1,13 @@
+const questions = [
+    "O que eu aprendi hoje?",
+    "O que me deixou aborrecido?",
+    "O que eu poderia fazer para melhorar?",
+    "O que me deixou feliz hoje?",
+    "Quantas pessoas ajudei hoje?",
+]
+
+const ask = (index = 0) => {
+    process.stdout.write(questions [index] + "\n\n\n")
+}
+
+ask()
