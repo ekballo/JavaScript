c9b6dbc4881a7afca64ffeb5f78f30353b14ce39
Jefferson Garcia <ekball007@outlook.com>
2022-08-22 08:13:00 -0300

    BestME

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -7,7 +7,38 @@ const questions = [
 ]
 
 const ask = (index = 0) => {
-    process.stdout.write(questions [index] + "\n\n\n")
+    process.stdout.write("\n" + questions [index] + " > ")
 }
 
 ask()
+
+const answers = []
+process.stdin.on("data", data => {
+    answers.push(data.toString().trim())
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    } else {
+        console.log(answers)
+        process.exit()
+    }
+})
+
+process.on('exit', () => {
+    console.log(`
+    
+        Bacana Jefferson!
+
+        O que você aprendeu hoje foi:
+        ${answers[0]}
+
+        O que te aborreceu e você poderia melhorar foi: 
+        ${answers[1]}
+
+        O que te deixo feliz hoje:
+        ${answers[2]}
+
+        Você ajudou ${answers[3]} pessoas hoje!!
+
+        Volte amanhã para mais reflexões!!
+    `)
+})
